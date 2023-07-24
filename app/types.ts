a7bec4b644b98ca4cdb7d06edf0b4b6f06062774
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-24 17:00:17 +0900

    Update: Type 이름 변경

--- app/types.ts
+++ app/types.ts
@@ -6,7 +6,7 @@ export interface Product {
     sale?: Number,
     isBest: Boolean,
     isNewest: Boolean,
-    category: categorys,
+    category: Category,
 }
 
 export type Category = '카테고리1' | '카테고리2' | '카테고리3'
