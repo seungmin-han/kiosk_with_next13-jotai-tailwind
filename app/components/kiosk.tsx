169aeccce2d50aa0d117bf00bf5ba79eaaaf10ce
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-24 13:12:50 +0900

    Feat: 키오스크 초기 화면

---
+++ app/components/kiosk.tsx
@@ -0,0 +1,13 @@
+import KioskLayout from "../layouts/kiostLayout"
+
+const screenClassName = 'text-center justify-between';
+
+export default function Main() {
+    return (
+        <KioskLayout screenClassName={screenClassName}>
+            <h1 className="text-8xl mt-32">Hello</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum ratione quis, deleniti similique earum magnam illo est architecto eum id odit consectetur cumque itaque necessitatibus optio reiciendis, rerum maxime? Exercitationem.</p>
+            <button className="mb-32 font-bold">클릭해주세요.</button>
+        </KioskLayout>
+    )
+}
