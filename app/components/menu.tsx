a2197ed64d925f12ef7bb7adc5a696fed673a23d
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-24 17:00:36 +0900

    Feat: 탭 메뉴 및 할인 전 가격 표시 추가

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -8,19 +8,26 @@ const screenClassName: String = '';
 
 export default function Menu() {
     const [products, setProducts] = useAtom(productAtom);
-    const CATEGORY: Category[] = ['카테고리1', '카테고리2', '카테고리3'];
+    const CATEGORY: Category[] = ['카테고리1', '카테고리2', '카테고리3', '카테고리4'];
     return (
         <KioskLayout screenClassName={screenClassName}>
-            <div id="menu-header" className="">
-                
+            <div id="menu-header" className="flex justify-between items-center px-[40px] h-12">
+                {
+                    CATEGORY.map((v: Category) => {
+                        return <div className="">{v}</div>
+                    })
+                }
             </div>
-            <div className="w-full flex flex-wrap self-center items-center">
+            <div className="w-full flex flex-wrap self-center items-baseline">
             {
                 products.map((v: Product) => {
                     return <div className="w-3/12 text-center p-4">
                         <div className="flex flex-col items-center">
                             <div className="w-[120px] h-[120px] bg-slate-500"></div>
                             <div>{v.name}</div>
+                            {
+                                v.sale ? <div><s>{ v.price }</s></div> : <></>
+                            }
                             <div>{v.price - (v.price * (v.sale/100))}</div>
                         </div>
                     </div>
