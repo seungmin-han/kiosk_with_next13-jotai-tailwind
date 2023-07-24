1c82c5b020adadfb9bb04ac4d69655f191637e0a
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-24 16:29:30 +0900

    Update: 메인 라우팅 처리

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,13 +1,16 @@
+'use client'
+import { useRouter } from "next/navigation";
 import KioskLayout from "../layouts/kiostLayout"
 
 const screenClassName: String = 'text-center justify-between';
 
 export default function Main() {
+    const router = useRouter()
     return (
         <KioskLayout screenClassName={screenClassName}>
             <h1 className="text-8xl mt-32">Hello</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum ratione quis, deleniti similique earum magnam illo est architecto eum id odit consectetur cumque itaque necessitatibus optio reiciendis, rerum maxime? Exercitationem.</p>
-            <button className="mb-32 font-bold">클릭해주세요.</button>
+            <button className="mb-32 font-bold" onClick={() => router.push('/menu')}>터치하여 메뉴 선택으로 이동</button>
         </KioskLayout>
     )
 }
