67523d912897f8906d8bbfe02b8c8b8b764b30b5
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-24 13:12:28 +0900

    Update: 컴포넌트 이름 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Kiosk from "./components/kiosk";
+import Main from "./components/kiosk";
 
 export default function Home() {
   return (
     <div className='w-screen h-screen flex justify-center items-center bg-slate-100 pt-[20px]'>
-      <Kiosk></Kiosk>
+      <Main></Main>
     </div>
   )
 }
