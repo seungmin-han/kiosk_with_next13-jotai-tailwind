d1c0246b1dba57e7aea6c84f1ba3d3f99e79ac2e
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-25 11:16:10 +0900

    Update: 키오스크 스크린 내 포탈 위치 지정

--- app/layouts/kiostLayout.tsx
+++ app/layouts/kiostLayout.tsx
@@ -5,9 +5,10 @@ export default function KioskLayout({
     screenClassName: String,
 }) {
   return (
-    <div className='w-screen h-screen flex justify-center items-center bg-slate-100 pt-[20px]'>
-      <div className="w-full h-full bg-black max-w-[800px] p-3 pb-12 shadow-2xl m-auto rounded-t-2xl">
+    <div id="wrap" className='w-screen h-screen flex justify-center items-center bg-slate-100 pt-[20px]'>
+      <div id="device" className="w-full h-full bg-black max-w-[800px] p-3 pb-12 shadow-2xl m-auto rounded-t-2xl">
         <div id="screen" className={`w-full h-full bg-white flex flex-col text-black p-4 shadow-inner rounded-md ${screenClassName}`}>
+          <div id="portal"></div>
           {children}
         </div>
       </div>
