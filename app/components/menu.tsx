c8064e21d2dc1b59c932941e0eb20d0584624fe0
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-25 11:16:54 +0900

    Feat: 카테고리별 필터링 및 포탈 테스트

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -3,24 +3,49 @@ import KioskLayout from "../layouts/kiostLayout"
 import { useAtom } from 'jotai';
 import { Product, Category } from '@/app/types';
 import { productAtom } from '@/store/atoms';
+import { useEffect, useState } from "react";
+import Modal from "./modal";
 const screenClassName: String = '';
 
 
 export default function Menu() {
-    const [products, setProducts] = useAtom(productAtom);
-    const CATEGORY: Category[] = ['카테고리1', '카테고리2', '카테고리3', '카테고리4'];
+    const [selectedIndex, setSelectedIndex] = useState<number>(0);
+    const [products, _] = useAtom(productAtom);
+    const [copiedProducts, setCopiedProducts] = useState(products);
+    const CATEGORY: string[] = ['신상품', '인기메뉴', '카테고리1', '카테고리2', '카테고리3'];
+
+    useEffect(() => {
+        switch (selectedIndex) {
+            case 0:
+                setCopiedProducts(() => products.filter(v => v.isNewest));
+                break;
+            case 1:
+                setCopiedProducts(() => products.filter(v => v.isBest));
+                break;
+            default:
+                setCopiedProducts(() => products.filter(v => v.category === `카테고리${selectedIndex - 1}`));
+                break;
+        }
+    }, [selectedIndex, products]);
+
     return (
         <KioskLayout screenClassName={screenClassName}>
-            <div id="menu-header" className="flex justify-between items-center px-[40px] h-12">
+            <div id="menu-header" className="flex justify-between items-center h-12">
                 {
-                    CATEGORY.map((v: Category) => {
-                        return <div className="" key={v}>{v}</div>
+                    CATEGORY.map((v: string, idx: number) => {
+                        return <div
+                            className={`cursor-pointer h-full w-1/5 text-center mx-4 ${idx === selectedIndex ? 'border-b-8 border-blue-900' : ''}`}
+                            key={v}
+                            onClick={()=>setSelectedIndex(idx)}
+                        >
+                            {v}
+                        </div>
                     })
                 }
             </div>
             <div className="w-full flex flex-wrap self-center items-baseline">
             {
-                products.map((v: Product) => {
+                copiedProducts.map((v: Product) => {
                     return <div className="w-3/12 text-center p-4" key={v.code}>
                         <div className="flex flex-col items-center">
                             <div className="w-[120px] h-[120px] bg-slate-500"></div>
@@ -34,6 +59,9 @@ export default function Menu() {
                 })
             }    
             </div>
+            <Modal>
+                { 'some Text' }
+            </Modal>
         </KioskLayout>
     )
 }
