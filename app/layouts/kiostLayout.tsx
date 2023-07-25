cf7608be05611770ce187680f910bac0b1fcba38
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-25 14:51:05 +0900

    Update: 모달 고정을 위해 스타일 지정

--- app/layouts/kiostLayout.tsx
+++ app/layouts/kiostLayout.tsx
@@ -7,8 +7,7 @@ export default function KioskLayout({
   return (
     <div id="wrap" className='w-screen h-screen flex justify-center items-center bg-slate-100 pt-[20px]'>
       <div id="device" className="w-full h-full bg-black max-w-[800px] p-3 pb-12 shadow-2xl m-auto rounded-t-2xl">
-        <div id="screen" className={`w-full h-full bg-white flex flex-col text-black p-4 shadow-inner rounded-md ${screenClassName}`}>
-          <div id="portal"></div>
+        <div id="screen" className={`relative w-full h-full bg-white flex flex-col text-black p-4 shadow-inner rounded-md ${screenClassName}`}>
           {children}
         </div>
       </div>
