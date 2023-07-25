30d3e52b02c5dfe39b49dae8eb76afb11288b360
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-25 09:12:37 +0900

    Update: 레이아웃 통합으로 코드 중복 제거

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -2,8 +2,6 @@ import Menu from "../components/menu";
 
 export default function ChoiceMenu() {
   return (
-    <div className='w-screen h-screen flex justify-center items-center bg-slate-100 pt-[20px]'>
-      <Menu></Menu>
-    </div>
+    <Menu></Menu>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import Main from "@/app/components/main";
 
 export default function Home() {
   return (
-    <div className='w-screen h-screen flex justify-center items-center bg-slate-100 pt-[20px]'>
-      <Main></Main>
-    </div>
+    <Main></Main>
   )
 }
