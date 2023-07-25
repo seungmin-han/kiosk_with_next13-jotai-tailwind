f6e612439c72f8ec743cfb830b2eef04669c0ebe
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-25 09:12:51 +0900

    Update: 메인화면 문구 수정* Hello -> Kiosk

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -8,7 +8,7 @@ export default function Main() {
     const router = useRouter()
     return (
         <KioskLayout screenClassName={screenClassName}>
-            <h1 className="text-8xl mt-32">Hello</h1>
+            <h1 className="text-8xl mt-32">Kiosk</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum ratione quis, deleniti similique earum magnam illo est architecto eum id odit consectetur cumque itaque necessitatibus optio reiciendis, rerum maxime? Exercitationem.</p>
             <button className="mb-32 font-bold" onClick={() => router.push('/menu')}>터치하여 메뉴 선택으로 이동</button>
         </KioskLayout>
