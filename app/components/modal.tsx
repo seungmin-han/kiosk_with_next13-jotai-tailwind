8525d19cf39655db412e1e197f54db8f619081a4
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-25 11:15:45 +0900

    Feat: 모달로 사용할 포탈 생성

---
+++ app/components/modal.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useEffect, useState } from "react"
+import { createPortal } from "react-dom";
+
+export default function Modal({ children }: { children: React.ReactNode }) {
+    const [mounted, setMounted] = useState<boolean>(false);
+
+    useEffect(() => {
+        setMounted(true); 
+    }, [])
+
+    return mounted
+        ? createPortal(<>{children}</>, document.querySelector('#portal') as HTMLSelectElement)
+        : <></>;
+}
