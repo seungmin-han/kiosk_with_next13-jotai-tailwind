e2501d94a61210f6d29deed96c3541c16531e662
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-26 15:53:09 +0900

    Fix: eslint

--- types.ts
+++ types.ts
@@ -1,23 +1,23 @@
-export type Category = `카테고리${number}`
+export type Category = `카테고리${number}`;
 
 export interface Product {
-    name: string;
-    price: number;
-    code: number;
-    isSoldOut: boolean;
-    sale: number;
-    isBest: boolean;
-    isNewest: boolean;
-    category: Category;
-    options: ProductOption[];
+	name: string;
+	price: number;
+	code: number;
+	isSoldOut: boolean;
+	sale: number;
+	isBest: boolean;
+	isNewest: boolean;
+	category: Category;
+	options: ProductOption[];
 }
 
 export interface ProductOption {
-    optionName: string,
-    optionKey: string,
-    options: string[] 
+	optionName: string;
+	optionKey: string;
+	options: string[];
 }
 
 export interface CartProduct extends Product {
-    selectedOptions: string[]
+	selectedOptions: string[];
 }
