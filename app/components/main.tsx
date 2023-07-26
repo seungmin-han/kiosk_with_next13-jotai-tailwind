ffb879cdf5b6db41dd3f385f95561e6ab21d426f
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-26 09:59:05 +0900

    Update: PC 일 때 hover 인터렉션 추가

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -10,7 +10,7 @@ export default function Main() {
         <KioskLayout screenClassName={screenClassName}>
             <h1 className="text-8xl mt-32">Kiosk</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum ratione quis, deleniti similique earum magnam illo est architecto eum id odit consectetur cumque itaque necessitatibus optio reiciendis, rerum maxime? Exercitationem.</p>
-            <button className="mb-32 font-bold" onClick={() => router.push('/menu')}>터치하여 메뉴 선택으로 이동</button>
+            <button className="mb-32 font-bold transition hover:scale-105" onClick={() => router.push('/menu')}>터치하여 메뉴 선택으로 이동</button>
         </KioskLayout>
     )
 }
