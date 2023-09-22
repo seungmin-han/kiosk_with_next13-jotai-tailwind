386f12f0b914470d17d56a7adae41ce715c27067
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-09-22 16:40:33 +0900

    Fix: prettier

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -1,36 +1,47 @@
 import { Product } from '@/types';
-import Image from "next/image";
+import Image from 'next/image';
 
-export default function productList({ toggleDetailView, products, realPrice }: { toggleDetailView: Function, products: Product[], realPrice: Function }) {
-    return (
-        <>
-            {
-            products.map((v: Product) => {
-                return <div
-                    className="w-3/12 text-center p-4"
-                    key={v.code}
-                    onClick={()=>toggleDetailView(v)}
-                >
-                    <div className="flex flex-col items-center">
-                        <div className="w-[120px] h-[120px] bg-slate-500">
-                            <Image
-                                src={`/assets/image${v.code}.jpg`}
-                                width={120}
-                                height={120}
-                                alt={`image${v.code}`}
-                                loading="eager"
-                            >
-                            </Image>
-                        </div>
-                        <div>{v.name}</div>
-                        {
-                            v.sale ? <div><s>{ v.price }</s></div> : <></>
-                        }
-                        <div>{realPrice(v)}</div>
-                    </div>
-                </div>
-            })
-            }
-        </>
-    )
+export default function productList({
+	toggleDetailView,
+	products,
+	realPrice,
+}: {
+	toggleDetailView: Function;
+	products: Product[];
+	realPrice: Function;
+}) {
+	return (
+		<>
+			{products.map((v: Product) => {
+				return (
+					<div
+						className="w-3/12 text-center p-4"
+						key={v.code}
+						onClick={() => toggleDetailView(v)}
+					>
+						<div className="flex flex-col items-center">
+							<div className="w-[120px] h-[120px] bg-slate-500">
+								<Image
+									src={`/assets/image${v.code}.jpg`}
+									width={120}
+									height={120}
+									alt={`image${v.code}`}
+									loading="eager"
+								></Image>
+							</div>
+							<div>{v.name}</div>
+							{v.sale ? (
+								<div>
+									<s>{v.price}</s>
+								</div>
+							) : (
+								<></>
+							)}
+							<div>{realPrice(v)}</div>
+						</div>
+					</div>
+				);
+			})}
+		</>
+	);
 }
