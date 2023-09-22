34ea32ec0f6870b37212dc9eee3da6af9eb83842
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-09-22 16:39:07 +0900

    Update: 컴포넌트 제거하고 페이지로 소스 이동

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-import Main from "@/app/components/main";
+'use client'
+import { useRouter } from "next/navigation";
+import KioskLayout from "@/app/layouts/kiostLayout"
 
-export default function Home() {
-  return (
-    <Main></Main>
-  )
+const SCREEN_CLASS_NAME: String = 'p-4 text-center justify-between';
+
+export default function Main() {
+    const router = useRouter()
+    return (
+        <KioskLayout screenClassName={SCREEN_CLASS_NAME}>
+            <h1 className="text-8xl mt-32">Kiosk</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum ratione quis, deleniti similique earum magnam illo est architecto eum id odit consectetur cumque itaque necessitatibus optio reiciendis, rerum maxime? Exercitationem.</p>
+            <button className="mb-32 font-bold transition hover:scale-105" onClick={() => router.push('/menu')}>터치하여 메뉴 선택으로 이동</button>
+        </KioskLayout>
+    )
 }
